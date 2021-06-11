d518d301da263b081606dd70efc3fb55023d9849
alice <alice.m.bailey@hotmail.co.uk>
2021-06-11 14:08:04 +0100

    first heroku commit

--- listen.js
+++ listen.js
@@ -1,5 +1,6 @@
 const { app } = require("./app.js");
+let port = process.env.PORT || 9090;
 
-app.listen(9090, () => {
-  console.log("Server is listening");
+app.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
 });
