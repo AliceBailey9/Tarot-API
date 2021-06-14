560fd5e02e6bf55f1f73479f8aae6a24307b4a61
alice <alice.m.bailey@hotmail.co.uk>
2021-06-14 17:16:31 +0100

    cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const apiRouter = require("./Routers/apiRouter");
+const cors = require("cors");
 
-const app = express();
+app.use(cors());
 app.use(express.json());
+const app = express();
 
 app.use("/api", apiRouter);
 
